f36ee3296f2b153dfbce25cb6cdd5bb61db3a17d
Sarah Griffiths <sarah.griffiths@nanoporetech.com>
2023-09-22 15:19:06 +0000

    pytest tidy

--- bin/workflow_glue/tests/test_run_plannotate.py
+++ bin/workflow_glue/tests/test_run_plannotate.py
@@ -1,4 +1,5 @@
 """Test run_plannotate.py."""
+from datetime import date, datetime
 import filecmp
 import os
 
@@ -25,10 +26,22 @@ def test_per_assembly(test_data, assembly_file, gbk_file, tmpdir):
     """Test per assembly function in run_plannotate outputs correct gbk."""
     retval = os.getcwd()
     os.chdir(tmpdir)
+
+    # add date to template
+    today = date.today()
+    x = datetime.strptime(str(today), '%Y-%m-%d')
+    fmt_date = x.strftime('%d-%b-%Y').upper()
+    input_gbk = f"{test_data}/{gbk_file}"
+    with open(input_gbk, "r") as inputfile:
+        expected_gbk = f"{inputfile.read()}".format(date=fmt_date)
+    with open("gbk_with_date.gbk", "w") as outfile:
+        outfile.write(expected_gbk)
+
+    # run plannotate
     run_plannotate.make_yaml('Default')
     assembly = f"{test_data}/{assembly_file}"
     run_plannotate.per_assembly(assembly, "barcode01")
-    expected_gbk = f"{test_data}/{gbk_file}"
-    gbk = f"{tmpdir}/barcode01.annotations.gbk"
+    gbk = "barcode01.annotations.gbk"
+    gbk_bool = filecmp.cmp(gbk, "gbk_with_date.gbk")
     os.chdir(retval)
-    assert filecmp.cmp(gbk, expected_gbk) is True
+    assert gbk_bool is True
